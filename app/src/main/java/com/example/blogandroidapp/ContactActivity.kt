3a4e685a95cea70c1f774f8d4266db43b820a4dc
mateusz <mateuszgeslowski3108@gmail.com>
2021-03-29 21:46:02 +0200

    Added parent Activity

--- app/src/main/java/com/example/blogandroidapp/ContactActivity.kt
+++ app/src/main/java/com/example/blogandroidapp/ContactActivity.kt
@@ -2,10 +2,20 @@ package com.example.blogandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        return
     }
 }
